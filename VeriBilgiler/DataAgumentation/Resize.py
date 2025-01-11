b53da50176b7bfd711c13e924d7da2b2a19240ec
batuhan bozkurt <batuhanbozkurt37@hotmail.com>
2025-01-11 15:05:37 +0300

    Resize.py konsol renk eklendi

--- VeriBilgiler/DataAgumentation/Resize.py
+++ VeriBilgiler/DataAgumentation/Resize.py
@@ -12,7 +12,7 @@ def resize_images(input_directory, output_directory, target_size=(224, 224)):
     image_files = [f for f in os.listdir(input_directory) if f.lower().endswith(('png', 'jpg', 'jpeg'))]
 
     if not image_files:
-        print(f"{input_directory} dizininde resim bulunamadı.")
+        print(f"\033[91m{input_directory} dizininde resim bulunamadı.\033[0m")
         return
 
     if not os.path.exists(output_directory):
@@ -27,12 +27,12 @@ def resize_images(input_directory, output_directory, target_size=(224, 224)):
                 img = img.convert("RGB")
                 img_resized = img.resize(target_size, Image.Resampling.LANCZOS)
                 img_resized.save(output_path)
-                print(f"Kaydedildi: {output_path}")
+                print(f"\033[91mKaydedildi: {output_path}\033[0m")
         except Exception as e:
-            print(f"Hata: {image_path} - {e}")
+            print(f"\033[91mHata: {image_path} - {e}\033[0m")  
 
 def process_directory(base_directory):
-    base_directory_224 = base_directory + "_224" 
+    base_directory_224 = base_directory + "_224"  
     if not os.path.exists(base_directory_224):
         os.makedirs(base_directory_224)
 
@@ -40,7 +40,7 @@ def process_directory(base_directory):
         for subdir in dirs:
             subdir_path = os.path.join(root, subdir)
             total_images = get_image_count(subdir_path)
-            print(f"{subdir_path} dizininde toplam {total_images} resim bulundu.")
+            print(f"\033[92m{subdir_path} dizininde toplam {total_images} resim bulundu.\033[0m") 
 
             if total_images > 0:
                 relative_path = os.path.relpath(subdir_path, base_directory)
@@ -48,13 +48,13 @@ def process_directory(base_directory):
                 
                 resize_images(subdir_path, output_directory)
             else:
-                print(f"{subdir_path} dizininde resim bulunamadı.")
+                print(f"\033[91m{subdir_path} dizininde resim bulunamadı.\033[0m")  
 
 def main():
     base_directory = input("Lütfen ana dizin yolunu girin: ").replace('"', '')
     
     if not os.path.exists(base_directory):
-        print("Geçerli bir dizin giriniz.")
+        print("\033[91mGeçerli bir dizin giriniz.\033[0m")
         return
 
     process_directory(base_directory)
